beb11fea8d92a6cbc30c4e23a229c9246a12b008
CodeForFunUIT <nghiathdev@gmail.com>
2023-05-27 16:57:25 +0700

    add activity transaction

--- src/controllers/activity_controller.js
+++ src/controllers/activity_controller.js
@@ -3,7 +3,17 @@ import NFT from "../models/nft.js";
 import User from "../models/user.js";
 import { activityType } from "../utility/enum.js";
 import HttpMethodStatus from "../utility/static.js";
+import { ethers } from "ethers";
+import {contractMarketPlace} from "../utility/contract.js"
+import dotenv from "dotenv";
+import axios from "axios";
 
+dotenv.config({path: '../../config/config.env'})
+
+const etherscanProvider = new ethers.providers.EtherscanProvider('goerli');
+const provider = new ethers.providers.JsonRpcProvider(
+  `https://goerli.infura.io/v3/${process.env.INFURA_ID}`
+);
 export const activityLikedByOther = async (req, res) => {
   try {
     const {walletAddress} = req.body
@@ -34,3 +44,44 @@ export const activityLikedByOther = async (req, res) => {
     return HttpMethodStatus.badRequest(res, `${error}`);
   }
 };
+
+
+export const activityTransaction = async (req, res) => {
+  try {
+    const {walletAddress} = req.body
+    const history = await etherscanProvider.getHistory(walletAddress);
+    let newHistory = [];
+    for(const tx of history){
+      
+      // console.log(tx['data'].substring(2,10))
+      // axios.get(`https://raw.githubusercontent.com/ethereum-lists/4bytes/master/signatures/${tx['data'].substring(2,10)}`).then(
+      //   (res) => {
+      //     if(res.status === 200){
+      //       console.log(tx.blockNumber)
+      //        console.log(res.data)
+      //        method = res.data
+      //       }
+          
+      //   }
+      // ).catch(error => {
+      //   console.log(tx.blockNumber)
+      //   console.log('error');
+      //   method = "Tranfer"
+      // });
+      newHistory.push({
+        'value': ethers.utils.formatEther(tx.value),
+        'from': tx.from,
+        'to': tx.to,
+        'timestamp': tx.timestamp,
+        'method': tx.data.substring(2,10),
+        'blockNumber': tx.blockNumber,
+      })
+    }
+
+
+    return HttpMethodStatus.ok(res, `get transfer success`, newHistory)
+  
+  } catch (e) {
+    return HttpMethodStatus.badRequest(res, `error on activity transfer ${e.message}`)
+  }
+}
--- src/routers/activity_router.js
+++ src/routers/activity_router.js
@@ -3,7 +3,9 @@ const router = express.Router();
 
 import {
     activityLikedByOther,
+    activityTransaction,
 } from "../controllers/activity_controller.js";
 
 router.post("/activityLikedByOther",activityLikedByOther)
+router.post("/activityTransaction",activityTransaction)
 export default router;
